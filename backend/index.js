1b06968dca7dfafa5ab8d7a78ba77b030773baa3
Bruno Martins <brunomart97@hotmail.com>
2021-10-24 20:56:47 -0300

    Backend base structure

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+// Config JSON response
+app.use(express.json());
+
+// Solve CORS
+app.use(cors({
+  credentials: true,
+  origin: 'https://localhost:3000'
+}));
+
+// Public folder for images
+app.use(express.static('public'));
+
+// Routes
+
+app.listen(5000);

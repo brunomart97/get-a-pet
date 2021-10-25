f23ee0e4a42ee3b5895fd8e3e108814de6b6dc54
Bruno Martins <brunomart97@hotmail.com>
2021-10-25 07:29:53 -0300

    Starting the user controller

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -2,6 +2,73 @@ const User = require('../models/User');
 
 module.exports = class UserController {
   static async register(req, res) {
-    res.json('Olá Get a Pet');
+    const {
+      name,
+      email,
+      phone,
+      password,
+      confirmPassword,
+    } = req.body;
+
+    // Validations
+    if(!name) {
+      res.status(422)
+      .json({
+        message: 'O nome é obrigatório'
+      });
+      return;
+    }
+
+    if(!email) {
+      res.status(422)
+      .json({
+        message: 'O e-mail é obrigatório'
+      });
+      return;
+    }
+
+    if(!phone) {
+      res.status(422)
+      .json({
+        message: 'O telefone é obrigatório'
+      });
+      return;
+    }
+
+    if(!password) {
+      res.status(422)
+      .json({
+        message: 'A senha é obrigatória'
+      });
+      return;
+    }
+
+    if(!confirmPassword) {
+      res.status(422)
+      .json({
+        message: 'A confirmação de senha é obrigatória'
+      });
+      return;
+    }
+
+    // Check if password confirmation is correct
+    if(password !== confirmPassword) {
+      res.status(422)
+      .json({
+        message: 'A senha e a confirmação de senha precisam ser iguais'
+      });
+      return;
+    }
+
+    // Check if user exists
+    const userExist = await User.findOne({ email: email });
+
+    if(userExist) {
+      res.status(422)
+      .json({
+        message: 'E-mail já cadastrado. Utilize outro e-mail'
+      });
+      return;
+    }
   }
 }

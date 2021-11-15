9dcfa1878d6711d277ca92c4699dc43d6b4306e6
Bruno Martins <brunomart97@hotmail.com>
2021-11-15 08:05:23 -0300

    Removing the user pet

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -139,6 +139,7 @@ module.exports = class PetController {
   static async getPetById(req, res) {
     const id = req.params.id;
 
+    // Check if ID is valid
     if(!ObjectId.isValid(id)) {
       res.status(422)
       .json({
@@ -162,5 +163,49 @@ module.exports = class PetController {
     .json({
       pet
     });
+  }
+
+  // Deleting oet by ID
+  static async removePetById(req, res) {
+    const id = req.params.id;
+
+    // Check if ID is valid
+    if(!ObjectId.isValid(id)) {
+      res.status(422)
+      .json({
+        message: 'ID inválido!'
+      });
+      return;
+    }
+
+    // Check if pet exists
+    const pet = await Pet.findOne({_id: id});
+
+    if(!pet) {
+      res.status(404)
+      .json({
+        message: 'Pet não encontrado!'
+      });
+      return;
+    }
+    
+    // Check if logged in user registered the pet
+    const token = getToken(req);
+    const user = await getUserByToken(token);
+
+    if(pet.user._id.toString() !== user._id.toString()) {
+      res.status(422)
+      .json({
+        message: 'Houve um problema em processar a sua solicitação. Tente novamente mais tarde!'
+      });
+      return;
+    }
+
+    await Pet.findByIdAndRemove(id);
+    res.status(200)
+    .json({
+      message: 'Pet removido com sucesso!'
+    });
+    return;
   }
 }

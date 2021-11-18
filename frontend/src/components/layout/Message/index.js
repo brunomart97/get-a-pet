a77993a9d61fbf47bec0630f9997973de8c32acf
Bruno Martins <brunomart97@hotmail.com>
2021-11-18 19:04:20 -0300

    Finishing the flash message component

--- frontend/src/components/layout/Message/index.js
+++ frontend/src/components/layout/Message/index.js
@@ -1,14 +1,31 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import bus from '../../../utils/bus';
 
 import styles from './styles.module.css';
 
 function Message() {
   const [type, setType] = useState("");
+  const [visibility, setVisibility] = useState(false);
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    bus.addListener('flash', ({message, type}) => {
+      setType(type);
+      setVisibility(true);
+      setMessage(message);
+
+      setTimeout(() => {
+        setVisibility(false);
+      }, 3000);
+    })
+  }, []);
 
   return(
-    <div className={`${styles.message} ${styles[type]} "success"`}>
-      Message
-    </div>
+    visibility && (
+      <div className={`${styles.message} ${styles[type]}`}>
+        {message}
+      </div>
+    )
   )
 }
 
--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -1,9 +1,16 @@
 import api from '../utils/api';
+
 import { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import useFlashMessage from './useFlashMessage';
 
 export default function useAuth() {
+  const { setFlashMessage } = useFlashMessage();
+
   async function register(user) {
+    let msgText = 'Cadastro realizado com sucesso!';
+    let msgType = 'success';
+
     try {
       const data = await api.post('/users/register', user)
       .then((response) => {
@@ -12,8 +19,11 @@ export default function useAuth() {
 
       console.log(data);
     } catch (error) {
-      console.log(error);
+      msgText = error.response.data.message;
+      msgType = 'error';
     }
+
+    setFlashMessage(msgText, msgType);
   }
 
   return { register };

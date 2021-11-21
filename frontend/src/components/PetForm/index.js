784c104e9490eeaf9ab9b71e6f267f1c622e2e48
Bruno Martins <brunomart97@hotmail.com>
2021-11-21 10:19:55 -0300

    Completion of the add pets screen

--- frontend/src/components/PetForm/index.js
+++ frontend/src/components/PetForm/index.js
@@ -16,20 +16,43 @@ function PetForm({
   const colors = ["Branco", "Preto", "Cinza", "Caramelo", "Mesclado"];
 
   function onFileChange(e) {
-    
+    setPreview(Array.from(e.target.files));
+    setPet({...pet, images: [...e.target.files]});
   }
-
+  
   function handleChange(e) {
-    
+    setPet({...pet, [e.target.name]: [...e.target.value]});
+  }
+  
+  function handleColor(e) {
+    setPet({...pet, color: [...e.target.options[e.target.selectedIndex].text]});
   }
 
-  function handleColor(e) {
-    
+  function submit(e) {
+    e.preventDefault();
+    handleSubmit(pet);
   }
 
   return(
     <section>
-      <form>
+      <form onSubmit={submit}>
+        <div className={styles.previewPet}>
+          {preview.length > 0 ?
+            preview.map((image, index) => (
+              <img
+                src={URL.createObjectURL(image)}
+                alt={pet.name}
+                key={`${pet.name}+${index}`}
+              />
+            )) : pet.images && pet.images.map((image, index) => (
+              <img
+                src={`${process.env.REACT_APP_API}/images/pets/${image}`}
+                alt={pet.name}
+                key={`${pet.name}+${index}`}
+              />
+            ))
+          }
+        </div>
         <Input
           text="Imagem do pet"
           type="file"

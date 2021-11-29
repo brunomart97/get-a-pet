085e6bdce0313f55967280ba47220867c31a1baa
Bruno Martins <brunomart97@hotmail.com>
2021-11-29 07:45:40 -0300

    Showing pets from home

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -17,7 +17,27 @@ function Home() {
 
   return(
     <section>
-      <h1>Home</h1>
+      <h1 className={styles.title}>Home</h1>
+      <p>Veja os detalhes de cada um e conheça o tutor deles.</p>
+      <div>
+        {pets.length > 0 && (
+          pets.map((pet) => (
+            <div>
+              <p>Imagem do pet</p>
+              <h3>{pet.name}</h3>
+              <p><span className="bold">Peso:</span> {pet.weight}kg</p>
+              {pet.available ? (
+                <Link to={`pet/${pet._id}`}>Mais detalhes</Link>
+              ) : (
+                <p>Adotado</p>
+              )}
+            </div>
+          ))
+        )}
+        {pets.length === 0 && (
+          <p>Não há pets disponíveis para adoção no momento!</p>
+        )}
+      </div>
     </section>
   )
 }

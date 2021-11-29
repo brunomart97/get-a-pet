4180df1963f9091040ee45e613e018f9072ad12f
Bruno Martins <brunomart97@hotmail.com>
2021-11-29 19:04:59 -0300

    Home styles

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -19,17 +19,20 @@ function Home() {
     <section>
       <h1 className={styles.title}>Home</h1>
       <p>Veja os detalhes de cada um e conheça o tutor deles.</p>
-      <div>
+      <div className={styles.petContainer}>
         {pets.length > 0 && (
           pets.map((pet) => (
-            <div>
-              <p>Imagem do pet</p>
+            <div className={styles.petCard}>
+              <div
+                style={{backgroundImage: `url(${process.env.REACT_APP_API}/images/pets/${pet.images[0]})`}}
+                className={styles.petCardImage}
+              ></div>
               <h3>{pet.name}</h3>
               <p><span className="bold">Peso:</span> {pet.weight}kg</p>
               {pet.available ? (
                 <Link to={`pet/${pet._id}`}>Mais detalhes</Link>
               ) : (
-                <p>Adotado</p>
+                <p className={styles.adoptedText}>Adotado</p>
               )}
             </div>
           ))

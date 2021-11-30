659bf4bb1ab91bb97381bdaea6c9c4528e3e8e55
Bruno Martins <brunomart97@hotmail.com>
2021-11-30 19:48:00 -0300

    Ending the adoption page

--- frontend/src/pages/MyAdoptions/index.js
+++ frontend/src/pages/MyAdoptions/index.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import api from '../../utils/api';
 
 import styles from './styles.module.css';
@@ -6,10 +7,10 @@ import styles from './styles.module.css';
 import RoundedImage from '../../components/RoundedImage';
 
 function MyAdoptions() {
-  const [pets, SetPets] = useState([]);
+  const [pets, setPets] = useState([]);
   const [token] = useState(localStorage.getItem('token') || '');
 
-  useeffects(() => {
+  useEffect(() => {
     api.get('/pets/myadoptions', {
       headers: {
         Authorization: `Bearer ${JSON.parse(token)}`
@@ -21,7 +22,37 @@ function MyAdoptions() {
   }, [token]);
 
   return(
-    <h1>Minhas Adoções</h1>
+    <section>
+      <h1 className={styles.title}>Minhas Adoções</h1>
+      <div className={styles.petListContainer}>
+        {pets.length > 0 && (
+          pets.map((pet) => (
+            <div key={pet.id} className={styles.petListRow}>
+              <RoundedImage
+                src={`${process.env.REACT_APP_API}/images/pets/${pet.images[0]}`}
+                alt={pet.name}
+                width="px75"
+              />
+              <div>
+                <span className={`bold ${styles.petName}`}>{pet.name}</span>
+                <p><span className="bold">Ligue para:</span> {pet.user.phone}</p>
+                <p><span className="bold">Fale com:</span> {pet.user.name}</p>
+              </div>
+              <div className={styles.actions}>
+                {pet.available ? (
+                  <p>Adoção em processo</p>
+                ) : (
+                  <p>Parabéns por concluir a adoção!</p>
+                )}
+              </div>
+            </div>
+          ))
+        )}
+        {pets.length === 0 && (
+          <p>Ainda não há adoções de pets.</p>
+        )}
+      </div>
+    </section>
   )
 }
 

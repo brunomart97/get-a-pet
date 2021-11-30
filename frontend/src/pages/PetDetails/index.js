16bd4cced12e3387bb388b73e64966852c156d30
Bruno Martins <brunomart97@hotmail.com>
2021-11-30 18:13:57 -0300

    Searching for pet by API

--- frontend/src/pages/PetDetails/index.js
+++ frontend/src/pages/PetDetails/index.js
@@ -7,9 +7,21 @@ import styles from './styles.module.css';
 import useFlashMessage from '../../hooks/useFlashMessage';
 
 function PetDetails() {
+  const [pet, setPet] = useState({});
+  const {id} = useParams();
+  const {setFlashMessage} = useFlashMessage();
+  const [token] = useState(localStorage.getItem('token') || '');
+
+  useEffect(() => {
+    api.get(`/pets/${id}`)
+    .then((response) => {
+      setPet(response.data.pet);
+    })
+  }, [id]);
+
   return(
     <section>
-      <h1 className={styles.title}>Detalhes do Pet</h1>
+      <h1 className={styles.title}>{pet.name}</h1>
     </section>
   )
 }
